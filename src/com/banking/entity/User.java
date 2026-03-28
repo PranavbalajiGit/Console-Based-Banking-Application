6a8bc7f1e38bda4b67e13cd377ed2af54a1f172c
PranavbalajiGit <pranavbalaji65690@gmail.com>
2026-03-28 23:15:40 +0530

    Boiler Plate for Structural Implementation

---
+++ src/com/banking/entity/User.java
@@ -0,0 +1,65 @@
+package com.banking.entity;
+
+public class User {
+    private String username;
+    private String password;
+    private String contactNumber;
+    private String role;
+    private float accountBalance;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public float getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(float accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+        return username.equals(user.username) && password.equals(user.password) && contactNumber.equals(user.contactNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username.hashCode();
+        result = 31 * result + password.hashCode();
+        result = 31 * result + contactNumber.hashCode();
+        return result;
+    }
+}
--- src/com/banking/main/Main.java
+++ src/com/banking/main/Main.java
@@ -2,6 +2,7 @@ package com.banking.main;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Welcome to the Banking Application!");
     }
 }
